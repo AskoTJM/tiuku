0dc351f38ba2c3c4703c3d622052e95875c88062
Asko T.J. Mattila <asko.mattila@gmail.com>
2020-09-04 21:38:52 +0300

    Docker-compose ja volumet toimii

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,23 @@
-FROM golang:1.15
-
-RUN go get -d github.com/gorilla/mux
+FROM golang:1.15.1
 
 WORKDIR /tiuku
+VOLUME  ["/tiuku"]
+
+RUN go get -d github.com/gorilla/mux
+#GORM V2 uses these
+#RUN go get -u gorm.io/gorm
+#RUN go get -u gorm.io/driver/mysql
+
+#GORM V1 uses these
+RUN go get -u github.com/jinzhu/gorm
+RUN go get -u github.com/jinzhu/gorm/dialects/mysql
+
 #COPY go.mod ./
 #RUN go mod download
-COPY . .
+#COPY . .
 
-RUN go build -o main
-EXPOSE 8080
-
-CMD ["./main"]
-#RUN tail -fn0 /dev/null
+#RUN go build -o main
+#EXPOSE 8080
+#ENTRYPOINT [ "go","./main" ]
+CMD [./main]
+#RUN /ail -fn0 /dev/null
--- go/database.go
+++ go/database.go
@@ -3,15 +3,56 @@ package swagger
 import (
 	"log"
 
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+/*
+type User struct {
+	ID       int
+	Username string
+}
+*/
+type UserModel struct {
+	ID      int `gorm:"primary_key"`
+	Name    string
+	Address string
+}
+
 func connectToDB() {
-	dsn := "testiuser:testipassword@tcp(db:3306)/tiukuDBharset=utf8mb4&parseTime=True&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	db, err := gorm.Open("mysql", "testiuser:testipassword@tcp(localhost:3306)/tiukuDB?charset=utf8")
 	if err != nil {
-		log.Printf("Error with Gorm connecting mySql!")
+		log.Printf("Connection failed to Open")
 	}
+	log.Printf("Connection Established")
+
+	db.CreateTable(&User{})
+
+	//dsn := "testiuser:testipassword@tcp(db:3306)/tiukuDB?charset=utf8&parseTime=True"
+	//db, err := gorm.Open(mysql, "testiuser:testipassword@/tiukuDB?charset=utf8&parseTime=True")
+	//gorm.Open("mysql", "testiuser:testipassword@tcp(127.0.0.1:3306)/tiukuDB?charset=utf8&parseTime=True")
+	//db, err := sql.Open("mysql", "testiuser:testipassword@/tiukuDB")
+	//dsn := "testiuser:testipassword@tcp(127.0.0.1:3306)/tiukuDB?charset=utf8mb4&parseTime=True&loc=Local"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	//db.Debug().DropTableIfExists(&UserModel{})
+	//Drops table if already exists
+	//db.Debug().AutoMigrate(&UserModel{})
+	//Auto create table based on Model
+
+	//fmt.Printf("...yay...")
+	//log.Printf("Connection Established")
+
+	//test := segment{Name: "Vilperi", Kukkuu: "No totta kai", createdAt: time.Now()}
+	//db.Create(&test)
 	//return db
+}
+
+func testCreate() {
+	//test := segment{Name: "Vilperi", Kukkuu: "No totta kai", createdAt: time.Now()}
+	//db.Create(&test)
 }
